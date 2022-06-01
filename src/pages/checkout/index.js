ce2c8a377a1dcc71a345a514d598656cd2ce397d
mobystudent <mobystudent@gmail.com>
2022-06-01 20:50:32 +0300

    Changed checkout component to function

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -5,55 +5,49 @@ import routerStore from '~s/router.store';
 import modalStore from '~s/modal.store';
 import styles from './checkout.module.styl';
 
-class Checkout extends React.Component {
-	constructor() {
-		super();
-	}
+const Checkout = observer(() => {
+	const {
+		checkout,
+		label,
+		title,
+		form,
+		input,
+		btn,
+		name,
+		wrap
+	} = styles;
+	const formBody = [];
 
-	render() {
-		const {
-			checkout,
-			label,
-			title,
-			form,
-			input,
-			btn,
-			name,
-			wrap
-		} = styles;
-		const formBody = [];
+	for (let field in checkoutStore.formData) {
+		const { labelId, title, type, value } = checkoutStore.formData[field];
 
-		for (let field in checkoutStore.formData) {
-			const { labelId, title, type, value } = checkoutStore.formData[field];
-
-			formBody.push(
-				<label className={ label } key={ labelId } htmlFor={ labelId }>
-					<span className={ name }>{ title }</span>
-					<input
-						className={ input }
-						type={ type }
-						id={ labelId }
-						value={ value }
-						onChange={ (event) => checkoutStore.change(field, event.target.value) }
-					/>
-				</label>
-			);
-		}
-
-		return (
-			<div className={ checkout }>
-				<h1 className={ title }>Checkout</h1>
-				<form className={ form } action='#'>
-					{ formBody }
-					<div className={ wrap }>
-						<button className={ btn } type='button' onClick={ () => routerStore.moveTo('cart') }>Back</button>
-						<button className={ btn } type='button' onClick={ () => modalStore.switch(true) }>Order</button>
-					</div>
-				</form>
-				{ modalStore.modal }
-			</div>
+		formBody.push(
+			<label className={ label } key={ labelId } htmlFor={ labelId }>
+				<span className={ name }>{ title }</span>
+				<input
+					className={ input }
+					type={ type }
+					id={ labelId }
+					value={ value }
+					onChange={ (event) => checkoutStore.change(field, event.target.value) }
+				/>
+			</label>
 		);
 	}
-}
 
-export default observer(Checkout);
+	return (
+		<div className={ checkout }>
+			<h1 className={ title }>Checkout</h1>
+			<form className={ form } action='#'>
+				{ formBody }
+				<div className={ wrap }>
+					<button className={ btn } type='button' onClick={ () => routerStore.moveTo('cart') }>Back</button>
+					<button className={ btn } type='button' onClick={ () => modalStore.switch(true) }>Order</button>
+				</div>
+			</form>
+			{ modalStore.modal }
+		</div>
+	);
+});
+
+export default Checkout;
